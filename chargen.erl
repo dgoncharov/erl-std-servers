641b6642e4f4559114471e1f26632aed33b83ab7
Dmitry Goncharov <dgoncharov@unison.com>
2009-05-21 13:31:10 +0400

    ref

--- chargen.erl
+++ chargen.erl
@@ -61,15 +61,17 @@ serve_conn(Sock, N) ->
     end.
 
 gen_seq(N) ->
-    Chars = lists:seq(32, 32 + 94),
-    Low = N rem 95,
-    Up = (N + 71) rem 95,
+    Maxchars = 95, % There are 95 ascii chars which can be printed.
+    Linelen = 72, % The number of chars on a line.
+    Chars = lists:seq($\s, $\s + Maxchars - 1),
+    Low = N rem Maxchars,
+    Up = (N + Linelen - 1) rem Maxchars,
 %    io:format("~p - ~p~n", [Low, Up]),
     L = case Up > Low of
     true ->
         lists:sublist(Chars, Low + 1, Up - Low + 1);
     false ->
-        L1 = lists:sublist(Chars, Low, 10000),
+        L1 = lists:sublist(Chars, Low, Maxchars),
 	L2 = lists:sublist(Chars, 1, Up),
 %	io:format("L1: ~p, L2: ~p~n", [L1, L2]),
         L1 ++ L2
